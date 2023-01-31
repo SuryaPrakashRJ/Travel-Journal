02869cf522d4377f0106f3e42e4bcb2e96d219c3
Surya Prakash <suryaaprakashrj@gmail.com>
2023-02-01 00:59:27 +0530

    initial commit

---
+++ my-travel-journal/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header flex bg-lightred justify-center space-x-4 py-10">
+        <img src="globe-icon.svg" alt="globe-icon"></img>
+        <h3 className='text-3'>my travel journal</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
